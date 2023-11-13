243f09565b4a4e7e682745ed264e72f452b7fefc
Tris <tristan.cole@morningdata.co.uk>
2023-11-13 18:00:48 +0000

    RabbitMQ Config Models

---
+++ RabbitMQ.LoadTester/MDL.ServiceBus.RabbitMQ/ConfigModels/ExchangeAdd.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace MDL.ServiceBus.ConfigModels
+{
+    /// <summary>
+    /// Config Information for adding a new RabbitMQ Exchange
+    /// </summary>
+    /// <remarks>Has the default settings</remarks>
+    public class ExchangeAdd : ConfigModelBase
+    {
+        [JsonProperty("vhost")]
+        public string VHost { get; set; }
+
+        [JsonProperty("name")]
+        public string ExchangeName { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; } = "direct";
+
+        [JsonProperty("auto_delete")]
+        public bool AutoDelete { get; set; } = false;
+
+        [JsonProperty("durable")]
+        public bool Durable { get; set; } = false;
+
+        [JsonProperty("internal")]
+        public bool Internal { get; set; } = false;
+
+        [JsonProperty("arguments")]
+        public string[] Arguments { get; set; } = new string[0];
+    }
+}
---
+++ RabbitMQ.LoadTester/MDL.ServiceBus.RabbitMQ/ConfigModels/VirtualHostAdd.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace MDL.ServiceBus.ConfigModels
+{
+    /// <summary>
+    /// Config Information for adding a new RabbitMQ Virtual Host
+    /// </summary>
+    public class VirtualHostAdd : ConfigModelBase
+    {
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("tags")]
+        public string Tags { get; set; }
+    }
+}
