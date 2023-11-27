bc8e614d9a5215f114e1ddbcae1e4fadeb255233
MEYHOUSE\Tristan.cole <tristan.cole@morningdata.co.uk>
2023-11-27 14:31:48 +0000

    NOVUS: Have a retry methods

--- RabbitMQ.LoadTester/RabbitMQ.LoadTester/NEW/Novus/ServiceBusManager.vb
+++ RabbitMQ.LoadTester/RabbitMQ.LoadTester/NEW/Novus/ServiceBusManager.vb
@@ -3,6 +3,8 @@ using MDL.ServiceBus;
 using RabbitMQ.Client.Events;
 using System;
 using System.Drawing.Text;
+using System.ComponentModel;
+using System.Threading;
 
 namespace RabbitMQ.LoadTester.NEW.Novus
 {
@@ -14,6 +16,19 @@ namespace RabbitMQ.LoadTester.NEW.Novus
         private string SubscriberQueuePassword;
         private Subscribe Subscriber;
 
+        private bool SetupOkay;
+
+        private BackgroundWorker bwSubscribe;
+
+        /// <summary>
+        /// Connection Subscribe Retry Max Times
+        /// </summary>
+        const int MaxRetry = 10;
+
+        /// <summary>
+        /// Connection Subscribe Retry interval (milliseconds)
+        /// </summary>
+        const int RetryInterval = 10000;
 
 
         /// <summary>
@@ -27,6 +42,7 @@ namespace RabbitMQ.LoadTester.NEW.Novus
         /// <param name="queueEvent"></param>
         public ServiceBusManager(string dataset, string novusUsername, string queuePassword, string serviceBusHost, ushort amqpPort, Action<BasicDeliverEventArgs> queueEvent)
         {
+
             // setting up the NOVUS client user account name to be used by the RabbitMQ Service Bus
             //NovusClientAccountName = PathHelper.GetUserName(dataset, novusUsername);
             // the password used to access the queue
@@ -46,8 +62,51 @@ namespace RabbitMQ.LoadTester.NEW.Novus
             // if there//s no password stored in the credential manager, do not subscribe to the service bus
             //if (String.IsNullOrEmpty(novusClientCredentials.Password)) return;
 
-            // setup NOVUS client as a service bus subscriber
-            Subscriber = new Subscribe(RabbitConfig, queuePassword, queueEvent);
+            //Use background worker to setup, on a loop if the User/Exchange/Vhost/Queue is not setup yet
+
+            bwSubscribe = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = false
+            };
+            bwSubscribe.DoWork += BwSubscribe_DoWork;
+            bwSubscribe.ProgressChanged += BwSubscribe_ProgressChanged;
+
+            bwSubscribe.RunWorkerAsync(queueEvent);
+
+        }
+
+        private void BwSubscribe_DoWork(Object sender, DoWorkEventArgs e)
+        {
+            this.SetupOkay = false;
+            int n = 0;
+            do
+            {
+                try
+                {
+                    // setup NOVUS client as a service bus subscriber
+                    Subscriber = new Subscribe(RabbitConfig, SubscriberQueuePassword, (Action<BasicDeliverEventArgs>)e.Argument);
+
+                    this.SetupOkay = true;
+                    bwSubscribe.ReportProgress(n, "Okay");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    bwSubscribe.ReportProgress(n, $"Failed: {ex.Message} {ex.InnerException?.Message}");
+                }
+
+                Thread.Sleep(RetryInterval);
+                n++;
+            } while (n < MaxRetry);
+
+            bwSubscribe.ReportProgress(MaxRetry, "Ended");
+
+        }
+
+        private void BwSubscribe_ProgressChanged(Object sender, ProgressChangedEventArgs e)
+        {
+            System.Diagnostics.Trace.TraceInformation(BLL.TimeHelp.GetTimeText() + "ServiceBusManager.BwSubscribe_ProgressChanged: {0} | {1}", e.ProgressPercentage, e.UserState);
         }
 
 
